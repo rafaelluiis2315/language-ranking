fbbb3f6474585f9711f1842f5a0367a851012755
rafaelluiis2315 <rafael.barreto@liferay.com>
2022-07-26 09:55:23 -0300

    primeiro commit

---
+++ src/test/java/br/com/rafael/linguagens/api/LinguagensApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.linguagens.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguagensApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
